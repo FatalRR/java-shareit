d29bf820f9220d6e9fd823e51a2f1b579ed7e4a0
Sergey Malyunov <FatalRR@yandex.ru>
2023-05-10 17:08:36 +0700

    feat: Encapsulation and remove equals and hashcode

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -3,27 +3,12 @@ package ru.practicum.shareit.item.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.Objects;
-
 @Data
 @AllArgsConstructor
 public class Item {
-    Integer id;
-    String name;
-    String description;
-    Integer ownerId;
-    Boolean available;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Item item = (Item) o;
-        return Objects.equals(id, item.id) && Objects.equals(name, item.name) && Objects.equals(description, item.description) && Objects.equals(ownerId, item.ownerId) && Objects.equals(available, item.available);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, description, ownerId, available);
-    }
+    private Integer id;
+    private String name;
+    private String description;
+    private Integer ownerId;
+    private Boolean available;
 }
