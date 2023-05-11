565cacf1e9c20ab3abbc010345256ff26ed96037
Sergey Malyunov <FatalRR@yandex.ru>
2023-05-11 16:47:29 +0700

    feat: add new message

--- src/main/java/ru/practicum/shareit/messages/LogMessages.java
+++ src/main/java/ru/practicum/shareit/messages/LogMessages.java
@@ -8,7 +8,8 @@ public enum LogMessages {
     TRY_REMOVE("Попытка удалить: {}"),
     ERROR_404("Ошибка 404: {}"),
     ERROR_400("Ошибка 400: {}"),
-    ERROR_409("Ошибка 409: {}");
+    ERROR_409("Ошибка 409: {}"),
+    ERROR_500("Ошибка 500: {}");
 
     private final String textLog;
 
