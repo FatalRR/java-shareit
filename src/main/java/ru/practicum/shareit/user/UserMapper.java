6144d2541791091562f3a552589aec08a51c2dfa
Sergey Malyunov <FatalRR@yandex.ru>
2023-05-27 06:34:12 +0700

    feat: new class

---
+++ src/main/java/ru/practicum/shareit/user/UserMapper.java
@@ -0,0 +1,31 @@
+package ru.practicum.shareit.user;
+
+import lombok.experimental.UtilityClass;
+import ru.practicum.shareit.user.dto.UserDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@UtilityClass
+public class UserMapper {
+    public static UserDto toDto(User user) {
+        return new UserDto(
+                user.getId(),
+                user.getName(),
+                user.getEmail());
+    }
+
+    public static User toEntity(UserDto userDto) {
+        User user = new User();
+        user.setId(user.getId());
+        user.setName(userDto.getName());
+        user.setEmail(userDto.getEmail());
+        return user;
+    }
+
+    public static List<UserDto> toDtoList(List<User> users) {
+        return users.stream()
+                .map(UserMapper::toDto)
+                .collect(Collectors.toList());
+    }
+}
