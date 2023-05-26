ea593399ca8490dd281ce209a1333185b88a5837
Sergey Malyunov <FatalRR@yandex.ru>
2023-05-27 06:34:41 +0700

    fix: change to Dto

--- src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.exception.AlreadyExistException;
 import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.messages.ExceptionMessages;
+import ru.practicum.shareit.user.dto.UserDto;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,19 +19,20 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
     @Override
-    public List<User> getAll() {
-        return userRepository.findAll();
+    public List<UserDto> getAll() {
+        return UserMapper.toDtoList(userRepository.findAll());
     }
 
     @Transactional
     @Override
-    public User saveUser(User user) throws AlreadyExistException {
-        return userRepository.save(user);
+    public UserDto saveUser(UserDto userDto) throws AlreadyExistException {
+        return UserMapper.toDto(userRepository.save(UserMapper.toEntity(userDto)));
     }
 
     @Transactional
     @Override
-    public User update(Integer userId, User user) {
+    public UserDto update(Integer userId, UserDto userDto) {
+        User user = UserMapper.toEntity(userDto);
         user.setId(userId);
         Optional<User> userTemp = userRepository.findById(userId);
         if (user.getName() == null) {
@@ -39,12 +41,12 @@ public class UserServiceImpl implements UserService {
         if (user.getEmail() == null) {
             user.setEmail(userTemp.get().getEmail());
         }
-        return userRepository.save(user);
+        return UserMapper.toDto(userRepository.save(user));
     }
 
     @Override
-    public User getById(Integer id) {
-        return userRepository.findById(id).orElseThrow(() -> new NotFoundException(ExceptionMessages.USER_NOT_FOUND));
+    public UserDto getById(Integer id) {
+        return UserMapper.toDto(userRepository.findById(id).orElseThrow(() -> new NotFoundException(ExceptionMessages.USER_NOT_FOUND)));
     }
 
     @Transactional
