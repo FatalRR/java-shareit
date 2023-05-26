e9f6e2b71026102769a9187fa28bfbe9bc2c0e5d
Sergey Malyunov <FatalRR@yandex.ru>
2023-05-27 06:33:48 +0700

    fix: remove validation

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,11 +1,8 @@
 package ru.practicum.shareit.user;
 
 import lombok.Data;
-import ru.practicum.shareit.messages.ExceptionMessages;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 
 @Data
 @Entity
@@ -16,11 +13,8 @@ public class User {
     private Integer id;
 
     @Column(name = "name", nullable = false)
-    @NotBlank(message = ExceptionMessages.EMPTY_NAME)
     private String name;
 
-    @NotBlank
-    @Email(message = ExceptionMessages.INCORRECT_EMAIL)
     @Column(name = "email", nullable = false)
     private String email;
 }
