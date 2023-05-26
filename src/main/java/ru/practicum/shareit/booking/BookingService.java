6e86de65db9325ca67d4e89f71b5f1a7bf6db26c
Sergey Malyunov <FatalRR@yandex.ru>
2023-05-27 00:56:08 +0700

    fix: change to BookingDto

--- src/main/java/ru/practicum/shareit/booking/BookingService.java
+++ src/main/java/ru/practicum/shareit/booking/BookingService.java
@@ -5,13 +5,13 @@ import ru.practicum.shareit.booking.dto.BookingDto;
 import java.util.List;
 
 public interface BookingService {
-    Booking getBookingById(Integer userId, Integer bookingId);
+    BookingDto getBookingById(Integer userId, Integer bookingId);
 
-    List<Booking> getByUserId(Integer userId, String state);
+    List<BookingDto> getByUserId(Integer userId, String state);
 
-    List<Booking> getByOwnerId(Integer userId, String state);
+    List<BookingDto> getByOwnerId(Integer userId, String state);
 
-    Booking approveBooking(Integer userId, Integer bookingId, boolean approve);
+    BookingDto approveBooking(Integer userId, Integer bookingId, boolean approve);
 
-    Booking addNewBooking(Integer userId, BookingDto bookingDto);
+    BookingDto addNewBooking(Integer userId, BookingDto bookingDto);
 }
